f2a3131115dfb3344b392826a8b2fb7ea7481ed4
Daniel Mateu <danielmateu86@gmail.com>
2022-11-17 14:21:50 +0100

    Ubicar y cambio de nombre de archivo

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+
 
 const { response } = require("express");
 
@@ -11,11 +14,25 @@ const cargarArchivo = (req, res = response) => {
     }
     
 
-    // console.log('req.files >>>', req.files); // eslint-disable-line
-
     const {archivo} = req.files;
 
-    const uploadPath = path.join( __dirname, '../uploads/',  archivo.name) ;
+    const nombreCortado = archivo.name.split('.');
+    const extension = nombreCortado[nombreCortado.length - 1];
+
+    // Validar la extensión
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+
+    if(!extensionesValidas.includes(extension)){
+        return res.status(400).json({
+            msg:`Extensión .${extension} no permitida, Extensiones válidas: ${extensionesValidas}`
+        });
+    }
+
+    // res.json({extension});
+
+    const nombreTemp = uuidv4() + '.' + extension; 
+
+    const uploadPath = path.join( __dirname, '../uploads/',  nombreTemp) ;
 
     archivo.mv(uploadPath, (err) => {
         if (err) {
