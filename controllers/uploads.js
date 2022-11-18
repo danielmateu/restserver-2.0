df32193942055eed68689b08b2520d8ec9615c1e
Daniel Mateu <danielmateu86@gmail.com>
2022-11-18 12:54:34 +0100

    Desestructurar de undefined

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,29 +1,64 @@
 
 const { response } = require("express");
 const { subirArchivo } = require("../helpers");
+const { validarArchivoSubir } = require("../middlewares");
+const { Usuario, Producto } = require("../models");
 
 
-const cargarArchivo = async(req, res = response) => {
+const cargarArchivo = async (req, res = response) => {
 
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-        res.status(400).json({msg: 'No hay archivos que subir.'});
-        return;
-    }
 
     try {
-            //txt, md
-            // const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos' );
-            const nombre = await subirArchivo(req.files, undefined, 'imgs' );
-            res.json({nombre})
-        
-    } catch (msg) {
-        res.status(400).json({msg})
-    }
-    
+        //txt, md
+        // const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos' );
+        const nombre = await subirArchivo(req.files, undefined, 'imgs');
+        res.json({ nombre })
 
+    } catch (msg) {
+        res.status(400).json({ msg })
+    }
 
 }
 
+const actualizarImagen = async (req, res = response) => {
+
+    
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({ msg: `No existe ningún usuario con el id ${id}` })
+            }
+            break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({ msg: `No existe ningún producto con el id ${id}` })
+            }
+            break;
+
+        default:
+            return res.status(500).json({ msg: 'Se me olvidó validar esto 🙃' })
+    }
+
+    const nombre = await subirArchivo(req.files, undefined, coleccion);
+    
+    modelo.img = nombre;
+
+    await modelo.save()
+
+    res.json({
+        modelo
+    })
+}
+
 module.exports = {
-    cargarArchivo
+    cargarArchivo,
+    actualizarImagen
 }
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,15 +1,23 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { cargarArchivo } = require('../controllers/uploads');
+const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
+const { coleccionesPermitidas } = require('../helpers');
 
-const { validarCampos } = require('../middlewares/validar-campos');
+const { validarCampos, validarArchivoSubir } = require('../middlewares');
 
 
 
 
 const router = Router();
 
-router.post('/', [],cargarArchivo)
+router.post('/', validarArchivoSubir,cargarArchivo);
+
+router.put('/:coleccion/:id', [
+    validarArchivoSubir,
+    check('id', 'El id debe ser un mongoID').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas(c,['usuarios', 'productos'])),
+    validarCampos,
+], actualizarImagen)
 
 
 
